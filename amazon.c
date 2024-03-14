b14b292e3258d90c3f57ef39d3f85ad0f3bf2d8c
Charles <charles.duval2806@gmail.com>
2024-03-14 16:09:17 +0100

    Liens amazon

--- amazon.c
+++ amazon.c
@@ -3,3 +3,5 @@
 //
 // https://www.amazon.fr/gp/product/B09NSTKSHW/ref=ox_sc_act_title_1?smid=A572SBHRZUAU5&th=1
 //https://www.amazon.fr/gp/product/B07D1XCKWW/ref=ox_sc_act_title_2?smid=A1X6FK5RDHNB96&psc=1
+
+//
